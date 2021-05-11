6da38096d3bc66a479d195b36419d3700961aa35
Aashil <aashil.themegrill@gmail.com>
2021-05-11 21:55:02 +0545

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import { Heading } from "./Heading";
+import { EmployeeList } from "./EmployeeList";
+
+export const Home = () => {
+  return (
+    <>
+      <div className="container mx-auto">
+        <h3 className="text-center text-3xl mt-20 text-base leading-8 text-black font-bold tracking-wide uppercase">
+          CRUD with React Context API and Hooks
+        </h3>
+        <Heading />
+        <EmployeeList />
+      </div>
+    </>
+  );
+};
